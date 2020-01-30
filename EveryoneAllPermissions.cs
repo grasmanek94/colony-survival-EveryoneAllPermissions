6098715c315c6c1477119a71f8279b83cc610286
Gamer_Z <github@gz0.nl>
2020-01-31 00:59:27 +0100

    Initial Commit

---
+++ EveryoneAllPermissions.cs
@@ -0,0 +1,40 @@
+﻿using Networking;
+
+namespace grasmanek94.EveryoneAllPermissions
+{
+    [ModLoader.ModManager]
+    public static class EveryoneAllPermissions
+    {
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerConnectedLate, "OnPlayerConnectedLate")]
+        static void OnPlayerConnectedLate(Players.Player player)
+        {
+            PermissionsManager.EnsureLoaded();
+            PermissionsManager.AddGroupToUser(null, player, "peasant");
+            PermissionsManager.AddGroupToUser(null, player, "localhost");
+            PermissionsManager.AddGroupToUser(null, player, "king");
+            PermissionsManager.AddGroupToUser(null, player, "god");
+            PermissionsManager.AddGroupToUser(null, player, "emperor");
+            PermissionsManager.AddGroupToUser(null, player, "godemperor");
+
+            
+            NetworkSteam networkSteam = NetworkWrapper.Network as NetworkSteam;
+            NetworkSteam.PlayerConnectionState playerConnectionState;
+            if (networkSteam.TryGetState(player.ID, out playerConnectionState))
+            {
+                if(RCONHandler.Authenticateds == null)
+                {
+                    RCONHandler.Authenticateds = new System.Collections.Generic.List<Pipliz.Networking.TCPWrapper>();
+                }
+
+                if (!RCONHandler.Authenticateds.Contains(playerConnectionState.TcpWrapper))
+                {
+                    RCONHandler.Authenticateds.Add(playerConnectionState.TcpWrapper);
+                }
+            };
+
+            PermissionsManager.Permission perm = new PermissionsManager.Permission("debug");
+
+            PermissionsManager.AddPermissionToUser(null, player, perm);
+        }
+    }
+}
