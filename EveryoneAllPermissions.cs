c93437c5b43efe6a9f6710babb1c31ed04ee6ae9
Gamer_Z <github@gz0.nl>
2020-02-03 23:44:27 +0100

    Fix an exception in RCON network interface

--- EveryoneAllPermissions.cs
+++ EveryoneAllPermissions.cs
@@ -1,14 +1,52 @@
 ﻿using Networking;
+using static ModLoader;
+using Pipliz;
 
 namespace grasmanek94.EveryoneAllPermissions
 {
-    [ModLoader.ModManager]
+    [ModManager]
     public static class EveryoneAllPermissions
     {
-        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerConnectedLate, "OnPlayerConnectedLate")]
+        [ModCallback(EModCallbackType.OnPlayerConnectedLate, "OnPlayerConnectedLate")]
         static void OnPlayerConnectedLate(Players.Player player)
         {
+            NetworkSteam networkSteam = NetworkWrapper.Network as NetworkSteam;
+            NetworkSteam.PlayerConnectionState playerConnectionState;
+            if (networkSteam != null && networkSteam.TryGetState(player.ID, out playerConnectionState))
+            {
+                if (playerConnectionState != null)
+                {
+                    if (RCONHandler.Authenticateds == null)
+                    {
+                        RCONHandler.Authenticateds = new System.Collections.Generic.List<Pipliz.Networking.TCPWrapper>();
+                    }
+
+                    if (!RCONHandler.Authenticateds.Contains(playerConnectionState.TcpWrapper))
+                    {
+                        RCONHandler.Authenticateds.Add(playerConnectionState.TcpWrapper);
+                    }
+                }
+                else
+                {
+                    Log.WriteWarning("EveryoneAllPermissions: invalid playerConnectionState, can be ignored on SinglePlayer");
+                }
+            }
+            else
+            {
+                Log.WriteWarning("EveryoneAllPermissions: invalid NetworkSteam, can be ignored on SinglePlayer");
+            }
+
             PermissionsManager.EnsureLoaded();
+
+            PermissionsManager.Permission perm_dbg = new PermissionsManager.Permission("debug");
+            PermissionsManager.Permission perm_localhost = new PermissionsManager.Permission("cheats.enable");
+
+            if (PermissionsManager.HasPermission(player, perm_dbg) &&
+                PermissionsManager.HasPermission(player, perm_localhost))
+            {
+                return;
+            }
+
             PermissionsManager.AddGroupToUser(null, player, "peasant");
             PermissionsManager.AddGroupToUser(null, player, "localhost");
             PermissionsManager.AddGroupToUser(null, player, "king");
@@ -16,25 +54,7 @@ namespace grasmanek94.EveryoneAllPermissions
             PermissionsManager.AddGroupToUser(null, player, "emperor");
             PermissionsManager.AddGroupToUser(null, player, "godemperor");
 
-            
-            NetworkSteam networkSteam = NetworkWrapper.Network as NetworkSteam;
-            NetworkSteam.PlayerConnectionState playerConnectionState;
-            if (networkSteam.TryGetState(player.ID, out playerConnectionState))
-            {
-                if(RCONHandler.Authenticateds == null)
-                {
-                    RCONHandler.Authenticateds = new System.Collections.Generic.List<Pipliz.Networking.TCPWrapper>();
-                }
-
-                if (!RCONHandler.Authenticateds.Contains(playerConnectionState.TcpWrapper))
-                {
-                    RCONHandler.Authenticateds.Add(playerConnectionState.TcpWrapper);
-                }
-            };
-
-            PermissionsManager.Permission perm = new PermissionsManager.Permission("debug");
-
-            PermissionsManager.AddPermissionToUser(null, player, perm);
+            PermissionsManager.AddPermissionToUser(null, player, perm_dbg);
         }
     }
 }
